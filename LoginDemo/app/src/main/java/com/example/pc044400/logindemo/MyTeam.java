b0b6020ce912e3d0fd79f37d95744f9dd25c61ac
jade <jadesis123@gmail.com>
2018-03-02 21:27:57 -0600

    Revert "Revert "2nd update""
    This reverts commit 051a19ca4dbe2a44bc2ddb15c8dfe8b690776be4.

---
+++ LoginDemo/app/src/main/java/com/example/pc044400/logindemo/MyTeam.java
@@ -0,0 +1,13 @@
+package com.example.pc044400.logindemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyTeam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_team);
+    }
+}
--- LoginDemo/app/src/main/java/com/example/pc044400/logindemo/UserReg.java
+++ LoginDemo/app/src/main/java/com/example/pc044400/logindemo/UserReg.java
@@ -1,14 +1,21 @@
 package com.example.pc044400.logindemo;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class UserReg extends AppCompatActivity {
@@ -18,6 +25,7 @@ public class UserReg extends AppCompatActivity {
     SQLiteDatabase db;
     private Button _btnreg, _btnlogin;
     EditText _txtfanme, _txtlname, _txtpass, _txtemail,_txtfname, _txtphone;
+    private Spinner _spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +39,61 @@ public class UserReg extends AppCompatActivity {
         _txtphone = (EditText)findViewById(R.id.txtphone);
         _txtemail = (EditText)findViewById(R.id.txtemail);
         _btnreg = (Button)findViewById(R.id.btnReg);
+        _spinner = (Spinner)findViewById(R.id.TeamList);
+        Spinner s = (Spinner) findViewById(R.id.TeamList);
+
+
+        DBHandler dab = new DBHandler(getApplicationContext());
+
+
+        final List<String> labels = dab.getAllTeams();
+
+
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, labels);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+
+        _spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // your code here
+
+                    if(position == labels.size() -1 )
+                    {
+                        Toast.makeText(getApplicationContext(), "Re-directing to team creation page", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(UserReg.this, TeamReg.class);
+
+                        startActivity(intent);
+
+                    };
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
 
         _btnreg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                db=openHelper.getWritableDatabase();
+                db = openHelper.getWritableDatabase();
                 String fname=_txtfname.getText().toString();
                 String lname=_txtlname.getText().toString();
                 String pass=_txtpass.getText().toString();
                 String email=_txtemail.getText().toString();
                 String phone=_txtphone.getText().toString();
-                insertdata(fname, lname,pass,email,phone);
+                String team = _spinner.getSelectedItem().toString();
+                insertdata(fname, lname,pass,email,phone,team);
                 Toast.makeText(getApplicationContext(), "register successfully", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(UserReg.this, MainActivity.class);
+                intent.putExtra("userName", email);
+                startActivity(intent);
 
             }
         });
@@ -53,7 +104,7 @@ public class UserReg extends AppCompatActivity {
 
 
 
-    public void insertdata(String fname, String lname, String pass, String email, String phone){
+    public void insertdata(String fname, String lname, String pass, String email, String phone, String team){
 
         ContentValues contentValues = new ContentValues();
         contentValues.put(DBHandler.COL_2, fname);
@@ -61,9 +112,14 @@ public class UserReg extends AppCompatActivity {
         contentValues.put(DBHandler.COL_4, pass);
         contentValues.put(DBHandler.COL_5, email);
         contentValues.put(DBHandler.COL_6, phone);
+        contentValues.put(DBHandler.COL_7, team);
+        contentValues.put(DBHandler.COL_8, " Enter your Bio!");
         long id = db.insert(DBHandler.TABLE_NAME, null, contentValues);
 
     }
+
+
+
 }
 
 
