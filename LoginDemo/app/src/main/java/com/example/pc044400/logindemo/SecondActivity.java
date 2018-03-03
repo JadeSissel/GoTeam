089dfd83dd2f626b2baa397d7cf7b3980c1b0ec4
jade <jadesis123@gmail.com>
2018-03-02 21:27:14 -0600

    2nd update
    2nd update

--- LoginDemo/app/src/main/java/com/example/pc044400/logindemo/SecondActivity.java
+++ LoginDemo/app/src/main/java/com/example/pc044400/logindemo/SecondActivity.java
@@ -1,9 +1,10 @@
 package com.example.pc044400.logindemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -12,9 +13,12 @@ public class SecondActivity extends AppCompatActivity {
 
     //logged in home page, profile page
 
-    private EditText UserName;
-    private EditText Password;
-    private Button Login;
+    private Button _btnGoTeam;
+    private Button _btnMyBio;
+    private Button _btnSubmitKudo;
+    private Button _btnMyTeam;
+
+
 
 
 
@@ -27,11 +31,69 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        _btnGoTeam = (Button)findViewById(R.id.btnGoTeam);
+        _btnMyBio = (Button)findViewById(R.id.btnMyBio);
+        _btnSubmitKudo = (Button)findViewById(R.id.btnSubmitKudo);
+        _btnMyTeam = (Button)findViewById(R.id.btnMyTeam);
 
-        TextView nameView = (TextView) findViewById(R.id.tvStatus);
 
-        nameView.setText("testing of the variable to commence");
-        String UserName = getIntent().getExtras().getString("userName");
-        nameView.setText("My Profile Page");
+
+        TextView nameView = (TextView) findViewById(R.id.tvWelcome);
+
+        final String UserName = getIntent().getExtras().getString("userName");
+        nameView.setText(UserName + "'s Profile Page " );
+
+
+        _btnMyBio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MyBio.class);//Intend to go to a new registration activity
+                intent.putExtra("userName", UserName);
+                startActivity(intent);
+
+            }
+        });
+
+        _btnGoTeam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, GoTeamStats.class);//Intend to go to a new registration activity
+
+                startActivity(intent);
+
+            }
+        });
+
+
+        _btnSubmitKudo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, SubmitKudo.class);//Intend to go to a new registration activity
+
+                startActivity(intent);
+
+            }
+        });
+
+        _btnMyTeam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MyTeam.class);//Intend to go to a new registration activity
+
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ LoginDemo/app/src/main/java/com/example/pc044400/logindemo/SubmitKudo.java
@@ -0,0 +1,13 @@
+package com.example.pc044400.logindemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubmitKudo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_kudo);
+    }
+}
